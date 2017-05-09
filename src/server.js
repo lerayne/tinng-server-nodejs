cd7f330dc64c81d389135d6f4e7fce083a509a85
Michael Yegorov <lerayne@gmail.com>
2017-05-09 11:35:06 +0300

    getting ready to real tinng server

--- src/server.js
+++ src/server.js
@@ -3,19 +3,13 @@
  * @flow
  */
 
+import 'babel-polyfill'
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-const PORT = process.env.port || 3001
-
 app.get('/', (req, res) => {
-    res.end('Hello app!')
-})
-
-app.get('/test1', (req, res) => {
-    //res.end(__dirname)
-    res.sendFile('/static/index.html', {root: __dirname})
+    res.end('TINNG server working')
 })
 
 io.on('connection', socket => {
@@ -31,33 +25,8 @@ io.on('connection', socket => {
     })
 })
 
+const PORT = process.env.PORT || 3001
+
 server.listen(PORT.toString(), () => {
     console.log(`Server listening on ${PORT}`)
 })
-
-/*
-class TestClass {
-
-    var1 = 0
-
-    constructor() {
-        this.var1 = 1
-    }
-
-    static var2 = {
-        var3: 3
-    }
-
-    method1(arg1: string, arg2?: bool) {
-        return 4
-    }
-
-    method2() {
-        this.method1('string', true)
-    }
-}
-
-const num: string = 'hello!'
-
-const test = new TestClass()
-test.method1('string')*/
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+/**
+ * Created by lerayne on 14.03.17.
+ */
+
+const webpack = require('webpack')
+const path = require('path')
+const getExternals = require('webpack-node-externals')
+
+module.exports = function (env) {
+
+    const babelOptions = {
+        babelrc: false,
+        presets: [
+            ["env", {
+                targets: {node: "current"}
+            }],
+            "stage-0",
+            "flow"
+        ]
+    }
+
+    return {
+        target: 'node',
+
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+
+        entry: path.join(__dirname, 'src', 'server.js'),
+
+        output: {
+            path: path.join(__dirname),
+            filename: 'tinng-server-node.js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: babelOptions
+                    }
+                }
+            ]
+        },
+
+        externals: [
+            {
+                config: 'require("./config.js")' // require config as separate file
+            },
+            getExternals({
+                whitelist: [
+                    /\.css$/i //CSS files whitelisted so we can omit them by loader
+                ]
+            })
+        ]
+    }
+}
